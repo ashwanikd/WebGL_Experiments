aa8ac36a59878b66e2257df8874ed3e4bc0cb2e8
ashwani <b160137@nitsikkim.ac.in>
2020-04-02 12:36:58 +0530

    initial webgl code done for DFS maze

--- Algorithms/GraphAlgorithms/Maze/DFGGraphics0.js
+++ Algorithms/GraphAlgorithms/Maze/DFGGraphics0.js
@@ -8,32 +8,45 @@
  */
 
 // vertex shader source
-var VSHADER_SRC =   'uniform mat4 u_xformMatrix;'+'\n'+
-                    'attribute vec4 a_Position;'+'\n'+
+var VSHADER_SRC =   'attribute vec4 a_Position;'+'\n'+
                     'attribute vec4 a_Color;'+'\n'+
                     'varying vec4 v_Color;'+'\n'+
                     'attribute float a_PointSize;'+'\n'+
                     'void main(){'+'\n'+
-                    '   gl_Position = u_xformMatrix * a_Position;'+'\n'+
+                    '   gl_Position = a_Position;'+'\n'+
                     '   gl_PointSize = a_PointSize;'+'\n'+
                     '   v_Color = a_Color;'+'\n'+
                     '}'+'\n';         
+
 // fragment shader source
 var FSHADER_SRC =   'precision mediump float;'+'\n'+
                     'varying vec4 v_Color;'+'\n'+
                     'void main(){'+'\n'+
                     '   gl_FragColor = v_Color;'+'\n'+
                     '}'+'\n';
+
 /**
  *@Variable a_Position stores the location of attribute a_Position
- *@Variable u_xformMatrix storeslocation of attribute u_xformMatrix
  *@Variable a_PointSize stores location of attribute a_PointSize
  *@Variable a_Color stores location of attribute a_Color
  */
 var a_Position;
-var u_xformMatrix;
 var a_PointSize;
 var a_Color;
+
+/**
+ *@Variable vertexBuffer buffer for vertices
+ */
+var vertexBuffer;
+
+var FSIZE;
+
+// webgl context
+var gl;
+
+/**
+ *@variable vertexBuffer
+ *@variable xformmatrixbuffer
 /**
  *@author Ashwani kumar dwivedi ashwanikd0123@gmail.com
  *@version 1.0
@@ -52,7 +65,7 @@ function main(){
     canvas.height = window.innerHeight;
     
     // getting webgl context from canvas
-    var gl = getWebGLContext(canvas);
+    gl = getWebGLContext(canvas);
     if(!gl){
         console.log('unable to retrive webgl context');
     }
@@ -62,10 +75,11 @@ function main(){
         console.log('unable to initialize shaders');
     }
     
+    // getting locations of attributes and uniform variables
     a_Position = gl.getAttribLocation(gl.program,'a_Position');
     a_PointSize = gl.getAttribLocation(gl.program,'a_PointSize');
-    u_xformMatrix = gl.getUniformLocation(gl.program,'u_xformMatrix');
     a_Color = gl.getAttribLocation(gl.program,'a_Color');
+    
     
     if(a_Position<0){
         console.log('unable to retrive location of a_Position from shader programs');
@@ -73,14 +87,52 @@ function main(){
     if(a_PointSize<0){
         console.log('unable to retrive location of a_PointSize from shader programs');
     }
-    if(u_xformMatrix<0){
-        console.log('unable to retrive location of u_xformMatrix from shader programs');
-    }
     if(a_Color<0){
         console.log('unable to retrive location of a_Color from shader programs');
     }
     
+    // initializing buffers
+    vertexBuffer = gl.createBuffer();
+    if(!vertexBuffer){
+        console.log('unable to create vertex buffer');
+    }
+    
+    // binding buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    
+    draw();
+}
+
+/**
+ *Data about all the shapes stored in this variable
+ *form of the data will be
+ *x coordinate,y coordinate,pointsize,color(RGB)
+ */
+var points = [0.0,0.0, 5.0,
+              1.0,0.0,0.0,
+              0.5,0.5, 5.0,
+              1.0,1.0,1.0];
+var n=2;
+
+
+function draw(){
     
     gl.clearColor(0.0,0.0,0.0,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    vertices = new Float32Array(points);
+    FSIZE = vertices.BYTES_PER_ELEMENT;
+    
+    gl.bufferData(gl.ARRAY_BUFFER,vertices,gl.STATIC_DRAW);
+    
+    gl.vertexAttribPointer(a_Position,2,gl.FLOAT,false,FSIZE*6,0);
+    gl.enableVertexAttribArray(a_Position);
+    
+    gl.vertexAttribPointer(a_PointSize,1,gl.FLOAT,false,FSIZE*6,FSIZE*2);
+    gl.enableVertexAttribArray(a_PointSize);
+    
+    gl.vertexAttribPointer(a_Color,3,gl.FLOAT,false,FSIZE*6,FSIZE*3);
+    gl.enableVertexAttribArray(a_Color);
+    
+    gl.drawArrays(gl.POINTS,0,n);
 }
